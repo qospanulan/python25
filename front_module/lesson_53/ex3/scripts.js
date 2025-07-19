2113d3bae4e6c22d503ec2b99a6b1ca4aff0e100
qospanulan <kospanovulan@gmail.com>
2025-07-19 21:26:54 +0500

    DRF 3: Update, Delete, Detail, serializers

---
+++ front_module/lesson_53/ex3/scripts.js
@@ -0,0 +1,23 @@
+const blogsElem = document.getElementById("blogs");
+
+async function loadData() {
+  response = await fetch("http://localhost:8000/blog/", {
+    method: "GET",
+  }).then((response) => response.json());
+
+  blogs = response;
+
+  for (i = 0; i < blogs.length; i++) {
+    const blogElem = document.createElement("div");
+    blogElem.className = "blog";
+    blogData = blogs[i];
+
+    blogElem.innerHTML = `
+    <p><a href="http://localhost:8000/blog/${blogData.id}/" >${blogData.name}</a></p>
+  `;
+
+    blogsElem.appendChild(blogElem);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", loadData);
