90ef16962aadcb936ab8e99c9b0a9490d03c4022
qospanulan <kospanovulan@gmail.com>
2025-07-31 20:36:14 +0500

    Advanced DRF 1: Service Layer

---
+++ django_module/django2/blog/services/blog_service.py
@@ -0,0 +1,75 @@
+from django.db.models import QuerySet
+
+from blog.models import Blog, Post
+
+
+class BlogService:
+
+    def get_all_blogs(self) -> list[Post]:
+
+        blogs: QuerySet[Blog] = Blog.objects.all()
+        # blogs: QuerySet[Blog] = Blog.objects.filter(
+        #     ~Q(status=HIDDEN)
+        # )
+
+        return blogs
+
+    def create_blog(
+            self,
+            user_id: int,
+            data: dict
+    ) -> Blog:
+
+        # blog = Blog(
+        #     name=data.get('name'),
+        #     description=data.get('description'),
+        #     tags=data.get('tags')
+        # )
+
+        blog = Blog(
+            author_id=user_id,
+            **data
+        )
+
+        blog.save()
+
+        return blog
+
+    def get_blog_by_id(self, blog_id: int) -> Blog:
+
+        blog: Blog = Blog.objects.get(id=blog_id)
+
+        return blog
+
+    def update_blog(
+            self,
+            blog_id: int,
+            data: dict
+    ) -> Blog:
+        blog: Blog = Blog.objects.get(id=blog_id)
+
+        for key, value in data.items():
+            setattr(blog, key, value)
+
+        blog.save()
+
+        return blog
+
+    def update_status(
+            self,
+            blog_id: int,
+            new_status: str
+    ) -> Blog:
+        blog: Blog = Blog.objects.get(id=blog_id)
+
+        blog.status = new_status
+
+        blog.save()
+
+        return blog
+
+    def delete_blog(self, blog_id: int) -> None:
+
+        blog: Blog = Blog.objects.get(id=blog_id)
+
+        blog.delete()
--- django_module/django2/blog/views.py
+++ django_module/django2/blog/views.py
@@ -1,63 +1,23 @@
-from django.db.models import QuerySet, Q
-from django.http import JsonResponse, HttpRequest
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.decorators import api_view
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import views, status, generics
+from rest_framework import views, status
 
-from blog.constants import HIDDEN
-from blog.models import Blog
 from blog.serializers import (
     BlogListOutputSerializer, BlogCreateInputSerializer,
     BlogCreateOutputSerializer, BlogDetailOutputSerializer,
     BlogFullUpdateInputSerializer, BlogStatusUpdateInputSerializer
 )
+from blog.services.blog_service import BlogService
 
-# === CLASS BASED VIEWS: GENERICS ======================================================
-
-class BlogListGenericAPIView(generics.ListCreateAPIView):
-    # queryset = Blog.objects.all()
-    # serializer_class = BlogListOutputSerializer
-
-    def get_queryset(self):
-        qs = Blog.objects.all()
-        # qs = qs.filter()
-        return qs
-
-    def get_serializer_class(self):
-        if self.request.method == "GET":
-            return BlogListOutputSerializer
-        else:
-            return BlogCreateOutputSerializer
-
-    def perform_create(self, serializer):
-        # serializer.validated_data['name'] = serializer.validated_data['name'].upper()
-        serializer.save()
-
-
-class BlogRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Blog.objects.all()
-    serializer_class = BlogDetailOutputSerializer
-    lookup_field = 'id'
-    lookup_url_kwarg = 'blog_id'
-
-
-# === CLASS BASED VIEWS: API VIEW ======================================================
 
 class BlogListAPIView(views.APIView):
 
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        blogs: QuerySet[Blog] = Blog.objects.all()
-        # blogs: QuerySet[Blog] = Blog.objects.filter(
-        #     ~Q(status=HIDDEN)
-        # )
 
-        print("== GET =========================================")
-        print(f"User: {request.user}")
-        print("===========================================")
+        service = BlogService()
+        blogs = service.get_all_blogs()
 
         serializer = BlogListOutputSerializer(blogs, many=True)
         return Response(serializer.data)
@@ -68,8 +28,11 @@ class BlogListAPIView(views.APIView):
 
         serializer.is_valid(raise_exception=True)
 
-        serializer.validated_data['author'] = request.user
-        blog: Blog = serializer.save()
+        service = BlogService()
+        blog = service.create_blog(
+            user_id=request.user_id,
+            data=serializer.validated_data
+        )
 
         return Response(
             BlogCreateOutputSerializer(instance=blog).data
@@ -80,20 +43,27 @@ class BlogDetailDeleteAPIView(views.APIView):
 
     def get(self, request, blog_id):
         try:
-            blog: Blog = Blog.objects.get(id=blog_id)
-        except Blog.DoesNotExist:
+
+            service = BlogService()
+
+            blog = service.get_blog_by_id(
+                blog_id=blog_id
+            )
+
+            serializer = BlogDetailOutputSerializer(
+                instance=blog
+            )
+
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+
+        except Exception as e:
             return Response(
                 {"detail": "No Blog matches the given query."},
                 status=status.HTTP_404_NOT_FOUND
             )
-
-        serializer = BlogDetailOutputSerializer(
-            instance=blog
-        )
-        return Response(
-            serializer.data,
-            status=status.HTTP_200_OK
-        )
 
     def put(self, request, blog_id):
 
@@ -102,15 +72,11 @@ class BlogDetailDeleteAPIView(views.APIView):
 
             serializer.is_valid(raise_exception=True)
 
-            blog: Blog = Blog.objects.get(id=blog_id)
-
-            for key, value in serializer.validated_data.items():
-                setattr(blog, key, value)
-
-            # blog.name = serializer.validated_data.get("name")
-            # blog.description = serializer.validated_data.get("description")
-
-            blog.save()
+            service = BlogService()
+            blog = service.update_blog(
+                blog_id=blog_id,
+                data=serializer.validated_data
+            )
 
             return Response(
                 BlogDetailOutputSerializer(
@@ -130,11 +96,11 @@ class BlogDetailDeleteAPIView(views.APIView):
 
             serializer.is_valid(raise_exception=True)
 
-            blog: Blog = Blog.objects.get(id=blog_id)
-
-            blog.status = serializer.validated_data.get("status")
-
-            blog.save()
+            service = BlogService()
+            blog = service.update_status(
+                blog_id=blog_id,
+                new_status=serializer.validated_data.get('status')
+            )
 
             return Response(
                 BlogDetailOutputSerializer(
@@ -150,56 +116,11 @@ class BlogDetailDeleteAPIView(views.APIView):
 
 
     def delete(self, request, blog_id):
-        blog: Blog = Blog.objects.get(id=blog_id)
 
-        blog.delete()
+        service = BlogService()
+        service.delete_blog(blog_id=blog_id)
 
         return Response(
             {"message": "Successfully deleted"},
             status=status.HTTP_204_NO_CONTENT
         )
-
-
-# === FUNCTION BASED VIEWS ======================================================
-@api_view(http_method_names=['GET'])
-def get_blog_list(request):
-    blogs: QuerySet[Blog] = Blog.objects.all()
-
-    # blogs_json = []
-
-    # for blog in blogs:
-    # blog_json = {
-    #     "name": blog.name,
-    #     "description": blog.description,
-    #     "created_at": blog.created_at,
-    #     "updated_at": blog.updated_at
-    # }
-
-    # blog_json = {}
-
-    # for field in blog._meta.get_fields():
-    #     if not field.is_relation:
-    #         blog_json[field.name] = getattr(blog, field.name)
-
-    # blog_json = model_to_dict(blog)
-
-    # blog_json = BlogListSerializer(blog).data
-    # blogs_json.append(blog_json)
-
-    # blogs_json = BlogListSerializer(blogs, many=True).data
-    return Response(
-        BlogListOutputSerializer(
-            blogs,
-            many=True
-        ).data
-    )
-
-
-@api_view(http_method_names=['GET', 'POST'])
-def test_health(request: HttpRequest):
-    if request.method == "GET":
-        return Response({"msg": "Hello World!"})
-
-    elif request.method == "POST":
-        print(request.data)
-        return Response({"msg": "Post method handled!"})
